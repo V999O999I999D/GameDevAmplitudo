74377f19453ae96b642e46de2d11af30631b5072
Andrija Musikic <andrija.musikic.zef2def@gmail.com>
2020-07-01 13:23:46 +0200

    inital commit

---
+++ Domaci 1/Assets/Scripts/LoseGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseGame : MonoBehaviour
+{
+    [SerializeField] private GameObject prco;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        prco.transform.position = new Vector2(-10f, 3f);
+    }
+}
---
+++ Domaci 1/Assets/Scripts/Movement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float put;
+    private int jumpsRemaining = 2;
+    
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector2(transform.position.x + put, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector2(transform.position.x - put, transform.position.y);
+        }
+    }
+
+    void Jump()
+    {
+        if (jumpsRemaining > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 jump = new Vector2(0, 30f);
+            GetComponent<Rigidbody2D>().velocity = jump;
+            jumpsRemaining--;
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        jumpsRemaining = 2;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+}
